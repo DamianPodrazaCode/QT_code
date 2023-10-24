e7907dfeb03cb279ddda92cae488bd4538bbf118
Damian Podraza <damian.podraza@gmail.com>
2023-10-24 20:08:00 +0200

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Concurrent_Thread/01_Run_A_Function_In_A_Thread/mainwindow.cpp
+++ QT_Concurrent_Thread/01_Run_A_Function_In_A_Thread/mainwindow.cpp
@@ -1,40 +1,32 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
-{
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow) {
     ui->setupUi(this);
 }
 
-MainWindow::~MainWindow()
-{
+MainWindow::~MainWindow() {
     delete ui;
 }
 
-void ConcurentThreadFun(MainWindow *mainW)
-{
+void ConcurentThreadFun(MainWindow *mainW) {
     qInfo() << "ConcurentThreadFun - START";
     int counter = 0;
-    while(mainW->t_running){
+    while(mainW->t_running) {
         qInfo() << "ConcurentThreadFun - RUNNING" << counter++;
         QThread::msleep(500);
     }
-
     qInfo() << "ConcurentThreadFun - END";
 }
 
-void MainWindow::on_pushButton_clicked()
-{
-    if (!t_start){
+void MainWindow::on_pushButton_clicked() {
+    if (!t_start) {
         t_start = true;
         ui->pushButton->setText("Stop Thread");
         t_running = true;
         ConcurentThread = QtConcurrent::run(ConcurentThreadFun, this);
         ConcurentThread.resume();
-
-    }else{
+    } else {
         t_start = false;
         ui->pushButton->setText("Start Thread");
         t_running = false;
