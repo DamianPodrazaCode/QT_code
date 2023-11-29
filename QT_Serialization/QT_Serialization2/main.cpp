6dfc0445ce27e7645094f1f950999566c51beaef
Damian Podraza <damian.podraza@gmail.com>
2023-11-29 16:07:42 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Serialization/QT_Serialization2/main.cpp
+++ QT_Serialization/QT_Serialization2/main.cpp
@@ -5,17 +5,23 @@
 
 // qDeleteAll dla klas
 
-void qDelteAll_() {
-    QList<test*> lTest;
+void qDeleteAll_() {
+    QList<test *> lTest;
     for (int i = 0; i < 10; ++i) {
         test *t = new test();
         t->setObjectName(QString::number(i));
         lTest.append(t);
     }
+
+    qDeleteAll(lTest); // usunięcie objektów, ale nie listy wskaźników
+    lTest.clear(); // dopiero teraz znika lista wskaźników
+
 }
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+
+    qDeleteAll_();
 
     return a.exec();
 }
