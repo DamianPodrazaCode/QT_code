ac99f2560696c54b44392b91c49df87681c88d0a
Damian Podraza <damian.podraza@gmail.com>
2023-11-29 16:24:41 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Serialization/QT_Serialization2/main.cpp
+++ QT_Serialization/QT_Serialization2/main.cpp
@@ -4,6 +4,10 @@
 #include <QList>
 
 // qDeleteAll dla klas
+// wypełnianie kontenerów
+// sortowanie w kontenerach
+// porównywanie kontenerów
+// kopiowanie kontenerów
 
 void qDeleteAll_() {
     QList<test *> lTest;
@@ -15,13 +19,51 @@ void qDeleteAll_() {
 
     qDeleteAll(lTest); // usunięcie objektów, ale nie listy wskaźników
     lTest.clear(); // dopiero teraz znika lista wskaźników
+}
 
+void fillCont_() {
+    QList<int> lista = {1, 2, 3, 4};
+    qInfo() << lista;
+    lista.fill(9);
+    qInfo() << lista;
+    lista.resize(15);
+    qInfo() << lista;
+    lista.fill(9);
+    qInfo() << lista;
+    std::fill(lista.begin(), lista.end(), 3);
+    qInfo() << lista;
+}
+
+void sortCont_() {
+    QList<int> list = {123, 432, 12, 65, 765, 98, 101, 234, 5674, 23423, 12, 334, 324, 876};
+    qInfo() << list;
+    std::sort(list.begin(), list.end());
+    qInfo() << list;
+    std::reverse(list.begin(), list.end());
+    qInfo() << list;
+}
+
+void compareCont_() {
+    QList<int> list1 = {123, 432, 12, 65, 765, 98, 101, 234, 5674, 23423, 12, 334, 324, 876};
+    QList<int> list2 = {223, 452, 62, 885, 65, 9, 11, 24, 45674, 223, 3212, 7334, 5324, 86};
+    QList<int> list3 = {123, 432, 12, 65, 765, 98, 101, 234, 5674, 23423, 12, 334, 324, 876};
+    qInfo() << list1;
+    qInfo() << list2;
+    qInfo() << std::equal(list1.begin(), list1.end(), list2.begin()); // false
+    qInfo() << std::equal(list1.begin(), list1.end(), list3.begin()); // true
+}
+
+void copyCont_() {
 }
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
-    qDeleteAll_();
+    // qDeleteAll_();
+    // fillCont_();
+    // sortCont_();
+    // compareCont_();
+    copyCont_();
 
     return a.exec();
 }
