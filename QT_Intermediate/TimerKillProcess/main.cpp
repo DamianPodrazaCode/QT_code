a6c3a273be4b2f306b0615be4b459c180b5581da
Damian Podraza <damian.podraza@gmail.com>
2023-12-02 15:25:32 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Intermediate/TimerKillProcess/main.cpp
+++ QT_Intermediate/TimerKillProcess/main.cpp
@@ -1,20 +1,23 @@
 #include <QCoreApplication>
 #include <QProcess>
 #include <QTimer>
+#include <QThread>
+
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
 
     QProcess proc;
-    proc.start("calc");
+    // aplikacja własna bo windowsowe mają jaką blokade że nie można sobie wyłączyć prosesu
+    proc.start("d:/__GitHub/DataVisualizer/DataVisualizer/release/DataVisualizer.exe", QStringList() << "\r\n");
     // proc.setProgram("notepad.exe");
     // proc.start();
     // proc.waitForStarted();
-    //uint32_t id = proc.processId();
+
     qInfo() << proc.program() << proc.processId();
 
-    QTimer::singleShot(3000, &proc, &QProcess::kill);
-    // proc.kill();
+    QTimer::singleShot(3000, &proc, &QProcess::terminate);
+    //proc.kill();
 
     return a.exec();
 }
