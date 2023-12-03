71b9690d6b2f67805fd42c030308ad4428a1d285
Damian Podraza <damian.podraza@gmail.com>
2023-12-03 12:44:30 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

---
+++ QT_Advanced/02_thread_asynchro/main.cpp
@@ -0,0 +1,24 @@
+#include <QCoreApplication>
+#include <QThread>
+#include <QTimer>
+
+// kod asynchrioniczy na jednym wątku
+
+void test() {
+    qInfo() << "test" << QThread::currentThread();
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    QThread::currentThread()->setObjectName("main");
+    qInfo() << "main start" << QThread::currentThread();
+
+    // timer nie uruchamia sie w osobnym wątku
+    QTimer timer;
+    timer.singleShot(3000, &test); //funkcja uruchamia się po 3 sec. nie blokując głównego wątku
+
+    qInfo() << "main end" << QThread::currentThread();
+
+    return a.exec();
+}
