5c97d41dc6bdfe49f40e922d1c61a19a21e0b424
Damian Podraza <damian.podraza@gmail.com>
2023-12-07 19:59:23 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

---
+++ QT_Advanced/10_QtConcurrent_run_wait/main.cpp
@@ -0,0 +1,26 @@
+#include <QCoreApplication>
+#include <QThread>
+#include <QThreadPool>
+#include <QtConcurrent>
+#include <QFuture>
+
+void test(QString name, int max) {
+    for (int i = 0; i < max; ++i) {
+        qInfo() << name << i << QThread::currentThread();
+    }
+}
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+    QThread::currentThread()->setObjectName("main");
+    qInfo() << QThread::currentThread();
+
+    QString name = "name";
+
+    //future jest poto żeby kontorlować trwanie wątków, void jest typem zwracanym przez funkcje a parametry są podawane za nazwą funkcji
+    QFuture<void> future = QtConcurrent::run(test, name, 5);
+    qInfo() << "za run";
+    future.waitForFinished();
+    qInfo() << "end";
+    return a.exec();
+}
