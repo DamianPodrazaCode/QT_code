3102d572b2fe66cb59fafdcbe0fbd4980f25c93d
Damian Podraza <damian.podraza@gmail.com>
2023-12-07 19:39:56 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Advanced/09_QtConcurret_Run/main.cpp
+++ QT_Advanced/09_QtConcurret_Run/main.cpp
@@ -1,4 +1,18 @@
 #include <QCoreApplication>
+#include <QThread>
+#include <QThreadPool>
+#include <QtConcurrent>
+
+void loop() {
+    for (int i = 0; i < 10; ++i) {
+        qInfo() << "loop " << i << "on: " << QThread::currentThread();
+    }
+}
+
+void test() {
+    qInfo() << "test: " << QThread::currentThread();
+    loop();
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
