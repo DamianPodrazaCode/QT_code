faadae2a662929e0bf473cce173a4da8a753eef9
Damian Podraza <damian.podraza@gmail.com>
2023-12-07 21:22:46 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Advanced/12_QtConcurrent_mapped/main.cpp
+++ QT_Advanced/12_QtConcurrent_mapped/main.cpp
@@ -1,7 +1,37 @@
 #include <QCoreApplication>
+#include <QDebug>
+#include <QFuture>
+#include <QFutureWatcher>
+#include <QList>
+#include <QtConcurrent>
+
+int do_map(int value) {
+    qInfo() << "do stuff: " << value << QThread::currentThread();
+    int num = value * 10;
+    return num;
+}
 
 int main(int argc, char *argv[]) {
     QCoreApplication a(argc, argv);
+
+    QList<int> list;
+    for (int i = 0; i < 100; ++i) {
+        list << i;
+    }
+
+    //QFutureWatcher<int> watcher; // potrzebny co connect
+    QFuture<int> future = QtConcurrent::mapped(list, &do_map);
+    qInfo() << "za mapped";
+    //watcher.setFuture(future);
+
+    //watcher.waitForFinished();
+    future.waitForFinished();
+    qInfo() << "za wait";
+
+    // wymusi wait
+    qInfo() << future.results();
+
+    qInfo() << list;
 
     return a.exec();
 }
