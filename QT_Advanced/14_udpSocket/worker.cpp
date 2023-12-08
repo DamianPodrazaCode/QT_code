9eb30bb767ab8b4f46554ade37404a1aafbc20a7
Damian Podraza <damian.podraza@gmail.com>
2023-12-08 18:55:34 +0100

    Signed-off-by: Damian Podraza <damian.podraza@gmail.com>

--- QT_Advanced/14_udpSocket/worker.cpp
+++ QT_Advanced/14_udpSocket/worker.cpp
@@ -7,16 +7,36 @@ Worker::Worker(QObject *parent) : QObject{parent} {
 }
 
 void Worker::start() {
+    if (!socket.bind(port)) {
+        qInfo() << socket.errorString();
+        return;
+    }
+    qInfo() << "Started UDP on" << socket.localAddress() << " : " << socket.localPort();
+    broadcast();
 }
 
 void Worker::stop() {
+    timer.stop();
+    socket.close();
+    qInfo() << "Stopped";
 }
 
 void Worker::timeout() {
+    QString dete = QDateTime::currentDateTime().toString();
+    QByteArray data = dete.toLatin1();
+    QNetworkDatagram datagram(data, QHostAddress::Broadcast, port); // rozsyłanie do wszystkich
+    qInfo() << "send:" << data;
+    socket.writeDatagram(datagram);
 }
 
 void Worker::readReady() {
+    while (socket.hasPendingDatagrams()) {
+        QNetworkDatagram datagram = socket.receiveDatagram();
+        qInfo() << "read " << datagram.data() << "from " << datagram.senderAddress() << " : " << datagram.senderPort();
+    }
 }
 
 void Worker::broadcast() {
+    qInfo() << "broadcasting ...";
+    timer.start();
 }
